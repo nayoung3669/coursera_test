5c52b2bf79aa02cfa29753aa8a1f161d86065600
Nayoung <skdud3669@gmail.com>
2023-01-03 23:48:14 -0800

    context Api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import logo from './logo.svg';
+import React, { useState, createContext, useContext } from 'react';
 import './App.css';
-import Btn from './components/Btn';
 import Heading from './components/Heading';
 
-import React, { useState } from 'react';
-
-
+const themeDefault = { border: '10px solid green' };
+const themeContext = createContext(themeDefault);
 
 function App() {
   const [word,setWord] = useState('Eat ');
+  const theme = useContext(themeContext);
+  console.log(theme)
 
+  
+  function handleClick() {
+    setWord('Stay ');
+  }
+  
   return (
     <div className="App">
       <Heading message={word + "at Little lemon"} />
+      <button onClick={handleClick}>Click me</button>
     </div>
 
   )
