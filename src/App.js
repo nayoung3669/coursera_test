83eb03e09da33183a2c3ffb20e74b719d1c79234
Nayoung <skdud3669@gmail.com>
2023-01-04 17:34:02 -0800

    react-player

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
+import ReactPlayer from 'react-player';
 
 import Homepage from './components/Homepage';
 import AboutMe from './components/AboutMe';
 
 
 function App() {
+
+  const vidUrl="";
   return (
     <div className='App'>
+      <ReactPlayer 
+        url={vidUrl}
+        playing={false}
+        volume={0.5}
+      />
       <nav className='nav'>
         <Link to="/" className="nav-item">Homepage</Link>
         <Link to="/about-me" className="nav-item">About me</Link>
