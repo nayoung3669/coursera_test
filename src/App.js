a0ad7fc148b4c37ee80f66652d2070b7d0a9ac34
Nayoung <skdud3669@gmail.com>
2023-01-23 17:05:47 -0800

    mouseTracker

--- src/App.js
+++ src/App.js
@@ -1,48 +1,69 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-export default function App() {
-  const [giftCard, setGiftCard] = useState(
-    {
-      firstName: "Jennifer",
-      lastName: "Smith",
-      text: "Free dinner for 4 guests",
-      valid: true,
-      instructions: "To use your coupon, click the button below.",
-    }
-  );
-
-  function spendGiftCard() {
-    setGiftCard(prevState => {
-      return {
-        ...prevState,
-        text: "Your coupon has been used.",
-        valid: false,
-        instructions: "Please visit our restaurant to renew your gift card",
-      }
+const withMousePosition = (WrappedCompoenent) => {
+  return (props) => {
+    const [mousePosition,setMousePosition] = useState ({
+    x: 0,
+    y: 0,
     });
+
+    useEffect(() => {
+      const handleMousePositionChange = (e) => {
+        setMousePosition({
+          x: e.clientX,
+          y: e.clientY,
+        });
+      };
+
+      window.addEventListener("mousemove",handleMousePositionChange)
+
+      return () => ( 
+        window.removeEventListener("mousemove",handleMousePositionChange)
+      ) 
+    }, []);
+
+    return <WrappedCompoenent {...props} mousePosition={mousePosition} />
+  };
+}
+
+const PanelMouseLogger = ({ mousePosition }) => {
+  if (!mousePosition) {
+    return null;
+  }
+  return (
+    <div className="PanelTracker">
+        <p>Mouse Position : </p>
+        <div>
+          <span>x: {mousePosition.x}</span>
+          <span>y: {mousePosition.y}</span>
+        </div>
+    </div>
+  );
+};
+
+const PointMouseLogger = ({ mousePosition }) => {
+  if (!mousePosition) {
+    return null;
+  }
+  return (
+    <p>
+      ({mousePosition.x} , {mousePosition.y})
+    </p>
+  )
   }
 
+const PanelMouseTracker = withMousePosition(PanelMouseLogger)
+const PointMouseTracker = withMousePosition(PointMouseLogger)
+
+function App() {
+
   return (
-    <div style={{padding: '40px'}}>
-      <h1>
-        Gift Card Page
-      </h1>
-      <h2>
-        Customer: {giftCard.firstName} {giftCard.lastName}
-      </h2>
-      <h3>
-        {giftCard.text}
-      </h3>
-      <p>
-        {giftCard.instructions}
-      </p>
-      {
-        giftCard.valid && (
-          <button onClick={spendGiftCard}>
-            Spend Gift Card
-          </button>
-        )
-      }
+    <div className="App">
+      <header className="Header">Mouse Tracker</header>
+      <PanelMouseTracker />
+      <PointMouseTracker />
     </div>
   );
 }
+
+export default App;
